06348dae2d21c905415445ef86b86f94ff83bdc9
Laravel Daily <info@laraveldaily.com>
2021-04-11 16:32:17 +0000

    QuickAdminPanel automatic commit

---
+++ config/panel.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'date_format'         => 'Y-m-d',
+    'time_format'         => 'H:i:s',
+    'primary_language'    => 'id',
+    'available_languages' => [
+        'id' => 'Bahasa / Indonesia',
+    ],
+];
---
+++ resources/lang/id/auth.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'failed'   => 'Email atau Katalaluan anda salah, sila cuba lagi!',
+    'throttle' => 'Terlalu banyak percubaan log masuk, sila cuba kembali dalam   :seconds saat',
+];
